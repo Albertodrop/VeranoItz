498e550d0ed6d453470397432f960de4a25e2384
Albertodrop <lf_alberto@hotmail.com>
2018-02-24 01:54:17 -0600

    Eliminacion

--- appsummer.dao/conexion.php
+++ appsummer.dao/conexion.php
@@ -12,7 +12,7 @@ class Conexion
     public function conectar()
     {
         // $pdo_conn = new PDO( 'mysql:host=sql110.epizy.com;dbname=epiz_21389800_dbitz_summer;charset=utf8;','epiz_21389800','6mPPGsucEGiY');
-        $pdo_conn = new PDO('mysql:host=localhost;dbname=dbitz_summer;charset=utf8;', 'root', 'Samiloko99');
+        $pdo_conn = new PDO('mysql:host=localhost;dbname=dbitz_summer;charset=utf8;', 'root', '');
          // $pdo_conn = new PDO( 'mysql:host=localhost;dbname=id4636351_dbitz_summer;charset=utf8;','id4636351_itz','199720031230');
         return $pdo_conn;
     }
